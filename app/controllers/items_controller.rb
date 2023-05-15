8449ed1efdecdae247021d8dfd2fe9ac4f3f0ae9
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-05-15 21:06:20 +0300

    commit tree c759f3ee3a9b9b2d46c49bbd31eaf0b248509846

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,43 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      if user
+        items = user.items
+        render json: items
+      else
+        render json: { error: 'User not found' }, status: :not_found
+      end
+    else
+      items = Item.all
+      render json: items, include: :user
+    end
   end
 
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  def create
+    user = User.find_by(id: params[:user_id])
+    if user
+      item = user.items.create(item_params)
+      render json: item, status: :created
+    else
+      render json: { error: 'User not found' }, status: :not_found
+    end
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: 'Item not found' }, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
 end
